b2fe383b58324032e8455c07cad386de7192fee0
Kaushik Dhameliya <dhameliyakaushik13@gmail.com>
2020-07-31 14:09:39 +0530

    Add files via upload

---
+++ html5.internationalization.example.js
@@ -0,0 +1,36 @@
+var languages = Array.from(document.getElementsByClassName('language'));
+var xhttp = new XMLHttpRequest();
+var langDocument = {};
+var langData = {};
+
+languages.forEach(function(value, index) {
+	languages[index].addEventListener('click', function() {
+		localStorage.setItem('lang', this.dataset.lang)
+		switchLanguage(this.dataset.lang);
+	});
+});
+xhttp.onreadystatechange = function() {
+	if (this.readyState === 4 && this.status === 200) {
+		debugger
+		langDocument = JSON.parse(langData);
+		processLangDocument();
+	}
+};
+function switchLanguage(language) {
+	xhttp.open('GET', '/public/assets/i18n/' + language + '.json', true);
+	xhttp.setRequestHeader('Content-type', 'application/json');
+	xhttp.send();
+}
+function processLangDocument() {
+	var tags = document.querySelectorAll('span,img,a,label,li,option,h1,h2,h3,h4,h5,h6,button');
+	Array.from(tags).forEach(function(value, index) {
+		debugger
+		var key = value.dataset.langkey;
+		if (langDocument[key]) value.innerText = langDocument[key];
+	});
+}
+
+$(document).ready(function () {
+	const a = localStorage.getItem('lang')
+	switchLanguage(a)
+});
